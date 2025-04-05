31c1c8bef3f88be3bc819ee776a54dc232271506
Bry091802 <bryanmaligaya3@gmail.com>
2025-04-05 19:16:21 +0800

    5 commit

--- frontend/components/charts/BarChartCases.vue
+++ frontend/components/charts/BarChartCases.vue
@@ -78,7 +78,7 @@ onMounted(() => {
 
 <template>
   <div class="rounded-2xl shadow-sm p-6 border border-gray-200">
-    <h2 class="text-xl font-semibold mt-4">Cases Report: Past  Months</h2>
+    <h2 class="text-xl font-semibold mt-4">Cases Report: Past 7 Months</h2>
 
    <!-- loading loading -->
     <div v-if="loading">Loading...</div>
---
+++ frontend/pages/test.vue
@@ -0,0 +1,3 @@
+<template>
+    <!-- /wala ito -->
+</template>
